8ac88b352b4aff094d46a56c4b5890132d70dac9
David M. Rivero G <davidmriverog@gmail.com>
2017-01-18 21:17:49 -0400

    avances efectuados

--- eje_03_herencia_abs/index.php
+++ eje_03_herencia_abs/index.php
@@ -28,16 +28,28 @@ class Unit
 
     public function attack($opponent)
     {
-        echo "<p> {$this->name} ataca a $opponent </p>";
+        echo "<p> {$this->name} golpea  a $opponent </p>";
     }
 }
 
 class Solder extends Unit
 {
-
+    public function attack($opponent)
+    {
+        echo "<p> {$this->name} corta  a $opponent </p>";
+    }
 }
 
 class Archer extends Unit
 {
-
+    public function attack($opponent)
+    {
+        echo "<p> {$this->name} dispara  a $opponent </p>";
+    }
 }
+
+$solder = new Solder('Atila');
+$solder->attack('El Cid Campeador');
+
+$solder = new Archer('Robin Hood');
+$solder->attack('Erick el Rojo');
