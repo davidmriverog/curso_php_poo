8d5a016c7a2042519e70cde95cafbdbba19caefd
David M. Rivero G <davidmriverog@gmail.com>
2017-01-24 21:16:02 -0400

    inicio del ejercicio nro. 13 uso de facade

---
+++ eje_13_facade/public/index.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MyCommunity;
+
+use MyCommunity\Weapons\BasicBow;
+use MyCommunity\Weapons\BasicSword;
+use MyCommunity\Weapons\FireBow;
+
+require '../vendor/autoload.php';
+
+Translator::set([
+    'BasicBowAttack'=>':unit dispara una flecha a :opponent',
+    'BasicSwordAttack'=>':unit ataca con la espada a :opponent',
+    'CrossBowAttack' => ':unit dispara una flecha potente a :opponent',
+    'FireBowAttack' => ':unit dispara una flecha de  fuego  a :opponent'
+]);
+
+$bronzearmor = new Armors\BronzeArmor;
+$silverarmor = new Armors\SilverArmor;
+
+$robin = new Unit('Robin Hood',new FireBow);
+$robin->setArmor($silverarmor);
+
+$atila = Unit::createSoldier()
+    ->setArmor($bronzearmor)
+    ->setWeapon(new BasicSword)
+    ->setShield();
+
+$robin->attack($atila);
+$atila->attack($robin);
+
