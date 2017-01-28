f354a36736376458369911865bef2b30893d2645
David M. Rivero G <davidmriverog@gmail.com>
2017-01-28 19:38:01 -0400

    prueba inicial del getter dinamico

--- eje_15_getset/public/index.php
+++ eje_15_getset/public/index.php
@@ -4,3 +4,8 @@ namespace MyCommunity;
 
 require '../vendor/autoload.php';
 
+$user = new User([
+    'full_name'=>'David Miguel Rivero Guite'
+]);
+
+echo "Sr. {$user->full_name}";
--- eje_15_getset/src/User.php
+++ eje_15_getset/src/User.php
@@ -4,5 +4,10 @@ namespace MyCommunity;
 
 class User
 {
-    //
+    public function __construct(array $attributes=[])
+    {
+        foreach ($attributes as $key => $value) {
+            $this->$key = $value;
+        }
+    }
 }
