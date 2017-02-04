a4ef61709612474267a9256b14c81255676e3638
David Rivero <davidmriverog@gmail.com>
2017-02-03 21:16:59 -0400

    ejemplo de sobrecarga de metodos para propiedades estaticas

--- eje_17_calllstatic/public/index.php
+++ eje_17_calllstatic/public/index.php
@@ -7,7 +7,27 @@ require '../vendor/autoload.php';
 
 class OneClass
 {
+
+    /**
+     * Esta metodo dispara un evento dentro de una instanciacion de la clase
+     * 
+     * @param  string $method 
+     * @param  array  $args  
+     * @return mixed
+     */
     public function __call($method,array $args)
+    {
+        var_dump($method,$args);
+    }
+
+    /**
+     * Esta metodo dispara un llamado al metodo estatico fuera de una instancia.
+     * 
+     * @param  string $method 
+     * @param  array  $args  
+     * @return mixed
+     */
+    public static function __callStatic($method,array $args)
     {
         var_dump($method,$args);
     }
@@ -16,4 +36,6 @@ class OneClass
 
 $oneClass = new OneClass;
 
-$oneClass->oneFunction();
+// $oneClass->oneFunction('Hello GoodBye','The Beatles');
+
+OneClass::yellowSubmarine();
