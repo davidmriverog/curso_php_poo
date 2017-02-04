222b10488ce2d7d3379920c56b187df348b0ac23
David Rivero <davidmriverog@gmail.com>
2017-02-03 21:06:31 -0400

    avances efectuados metodos magicos

--- eje_17_calllstatic/public/index.php
+++ eje_17_calllstatic/public/index.php
@@ -4,3 +4,16 @@ namespace MyCommunity;
 
 require '../vendor/autoload.php';
 
+
+class OneClass
+{
+    public function __call($method,array $args)
+    {
+        var_dump($method,$args);
+    }
+}
+
+
+$oneClass = new OneClass;
+
+$oneClass->oneFunction();
